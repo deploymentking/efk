857f1810b789788d663375c41acfbe4b0a5d9acd
Lee Myring <mail@thinkstack.io>
2018-02-22 15:56:59 +0000

    Pass new env var LOGGER_FILE_PATH to td-agent service

--- via-td-agent/executables/entrypoint.sh
+++ via-td-agent/executables/entrypoint.sh
@@ -5,15 +5,17 @@ export FLUENTD_AGGREGATOR_ADDR=${FLUENTD_AGGREGATOR_ADDR}
 export FLUENTD_AGGREGATOR_PORT=${FLUENTD_AGGREGATOR_PORT}
 export FLUENTD_SOURCE_PROJECT=${FLUENTD_SOURCE_PROJECT}
 export FLUENTD_TAIL_TAG=${FLUENTD_TAIL_TAG}
+export LOGGER_FILE_PATH=${LOGGER_FILE_PATH}
 TD_AGENT_OPTIONS="-c /etc/td-agent/${FLUENTD_CONF}"
 EOF
 
 chown td-agent:td-agent /etc/default/td-agent
 service td-agent restart
 
-nohup java -Dlogger.loopCount=${LOGGER_LOOP_COUNT} \
-    -Dlogger.sleep=${LOGGER_THREAD_SLEEP} \
-    -cp ./java-logger-${VERSION_JAVA_LOGGER}.jar \
-    io.thinkstack.logger.slf4j.${LOGGER_ENTRY_POINT} &
+nohup java -Dlogger.filePath=${LOGGER_FILE_PATH} \
+           -Dlogger.loopCount=${LOGGER_LOOP_COUNT} \
+           -Dlogger.sleep=${LOGGER_THREAD_SLEEP} \
+           -cp /bin/java-logger-${VERSION_JAVA_LOGGER}.jar \
+           io.thinkstack.logger.slf4j.${LOGGER_ENTRY_POINT} &
 
 tail -F /var/log/td-agent/td-agent.log
