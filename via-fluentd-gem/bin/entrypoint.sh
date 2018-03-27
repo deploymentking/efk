430d42559b6000bbc80c77f720c0cf9c4418fbed
Lee Myring <mail@thinkstack.io>
2018-03-27 19:10:02 +0100

    Add container that runs fluent as a gem

---
+++ via-fluentd-gem/bin/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+export FLUENTD_AGGREGATOR_ADDR=${FLUENTD_AGGREGATOR_ADDR}
+export FLUENTD_AGGREGATOR_PORT=${FLUENTD_AGGREGATOR_PORT}
+export FLUENTD_SOURCE_PROJECT=${FLUENTD_SOURCE_PROJECT}
+export FLUENTD_TAIL_TAG=${FLUENTD_TAIL_TAG}
+export LOGGER_FILE_PATH=${LOGGER_FILE_PATH}
+
+nohup java -Dlogger.filePath=${LOGGER_FILE_PATH} \
+           -Dlogger.loopCount=${LOGGER_LOOP_COUNT} \
+           -Dlogger.sleep=${LOGGER_THREAD_SLEEP} \
+           -cp /bin/java-logger-${LOGGER_VERSION}.jar \
+           io.thinkstack.logger.slf4j.${LOGGER_ENTRY_POINT} &
+
+fluentd
