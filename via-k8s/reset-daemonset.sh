c17aa4f8d5e40ebaaa5e06756f6718698f66f0a0
Lee Myring <mail@thinkstack.io>
2018-04-09 12:27:04 +0100

    Fix parsing of Redis logs in k8s setup

--- via-k8s/reset-daemonset.sh
+++ via-k8s/reset-daemonset.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+echo "${green}Deleting existing config setup...${reset}"
 kubectl delete ds fluent-bit --namespace=logging
 kubectl delete configmap fluent-bit-config --namespace=logging
 sleep 5
 kubectl apply -f ./config/fluent-bit-configmap.yaml --namespace=logging
 sleep 5
 kubectl apply -f ./config/fluent-bit-ds-minikube.yaml --namespace=logging
+echo
+echo "${green}Tailing fluent-bit logs...${reset}"
+kubectl logs -f --namespace=logging $(kubectl get pods --namespace=logging -l k8s-app=fluent-bit-logging -o name) -c fluent-bit
