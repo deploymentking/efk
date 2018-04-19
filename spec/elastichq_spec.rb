374e31d0c9912767610aea34766f7f443ed9048b
Lee Myring <mail@thinkstack.io>
2018-04-19 16:12:26 +0100

    Update test to cater for new elasticHQ image

--- spec/elastichq_spec.rb
+++ spec/elastichq_spec.rb
@@ -7,25 +7,22 @@ describe 'ElasticHQ Installation' do
     it { should have_label 'Author' => 'Lee Myring <mail@thinkstack.io>' }
     it { should have_label 'Description' => 'ElasticHQ instance' }
     it { should have_maintainer 'Lee Myring' }
-    it { should have_cmd %w[python application.py --port 5000 --host 0.0.0.0] }
-    it { should have_expose '5000' }
     it { should have_user 'nobody' }
-    it { should have_workdir '/elasticHQ' }
 
     its(:arch) { should eq 'amd64' }
     its(:os) { should eq 'linux' }
 
     describe docker_run(described_image) do
-      describe file('/elasticHQ') do
+      describe file('/src') do
         it { should be_directory }
       end
 
-      describe file('/elasticHQ/requirements.txt') do
+      describe file('/src/requirements.txt') do
         it { should be_file }
         it { should contain 'jmespath==0.9.3' }
       end
 
-      %w[git bash].each do |package|
+      %w[bash gcc].each do |package|
         describe "Describe package #{package}" do
           describe package(package) do
             it { should be_installed }
@@ -37,10 +34,8 @@ describe 'ElasticHQ Installation' do
         end
       end
 
-      describe process('python') do
-        it { should be_running }
-        its(:user) { should eq 'root' }
-        its(:args) { should include 'application.py --port 5000 --host 0.0.0.0' }
+      describe command('pidof supervisord') do
+        its(:stdout) { should match /1/ }
       end
 
       describe port(5000) do
