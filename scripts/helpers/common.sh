3a7394f60ed1baf314549eda53ee6eb3399ef645
Lee Myring <mail@thinkstack.io>
2018-04-20 09:43:04 +0100

    Refactor restartContainer into module function

--- scripts/helpers/common.sh
+++ scripts/helpers/common.sh
@@ -8,17 +8,8 @@ reset=`tput sgr0`
 kibanaCurlPrefix="curl -u kibana:kibana"
 kibanaUrl="http://localhost:5601"
 
-function clearDockerLog {
-    dockerLogFile=$(docker inspect $1 | grep -G '\"LogPath\": \"*\"' | sed -e 's/.*\"LogPath\": \"//g' | sed -e 's/\",//g');
-    rmCommand="rm $dockerLogFile";
-    screen -d -m -S dockerlogdelete ~/Library/Containers/com.docker.docker/Data/com.docker.driver.amd64-linux/tty;
-    screen -S dockerlogdelete -p 0 -X stuff "$rmCommand";
-    screen -S dockerlogdelete -p 0 -X stuff '\n';
-    screen -S dockerlogdelete -X quit
-}
-
+# Check if Minikube, kubectl and Virtualbox and any other essential tool are installed
 function checkPreRequisites {
-    # Check if Minikube, kubectl and Virtualbox are installed
     echo
     echo "${green}Checking pre-requisites...${reset}"
     type docker >/dev/null 2>&1 || { echo >&2 "${yellow}Docker required but it's not installed.  Aborting.${reset}"; exit 1; }
@@ -27,6 +18,7 @@ function checkPreRequisites {
     type minikube >/dev/null 2>&1 || { echo >&2 "${yellow}Minikube required but it's not installed.  Aborting.${reset}"; exit 1; }
 }
 
+# Wait for a container to be running before continuing processing
 function continueAfterContainerCreated {
     cmd="kubectl get --no-headers pods --namespace=$1 --selector $2 -o=custom-columns=:.status.phase"
     echo
@@ -38,9 +30,37 @@ function continueAfterContainerCreated {
     echo
 }
 
+# Restart a running container, wait for it to respond on open port and then tail logs since the restart
+function restartContainer {
+    timestamp=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+    directory=$(cd `dirname $0` && pwd)
+    container=$1
+    port=$2
+
+    docker-compose -p efk -f docker-compose.yml restart ${container}
+
+    while ! nc -z localhost ${port} </dev/null; do sleep 5; done
+
+    docker logs --follow --since ${timestamp} fluentd
+}
+
+# Given the start of the curl command and the URL, wait for the curl to return successfully
+function checkURL {
+    echo "${green}Waiting for URL $2${reset}"
+    until $(eval '$1 --output /dev/null --silent --head --fail $2'); do
+        printf '.'
+        sleep 5
+    done
+    echo
+}
+
 # From https://github.com/elastic/kibana/issues/3709
 function createKibanaIndices {
     set -euo pipefail
+
+    content_type="Content-Type: application/json"
+    kbn_xsrf_header="kbn-xsrf: anything"
+    action=POST
 
     for index_pattern in agent-* \
                          apache-* \
@@ -52,25 +72,19 @@ function createKibanaIndices {
     do
         echo
         echo "${green}Creating index pattern $index_pattern...${reset}"
-        ${kibanaCurlPrefix} -f -X POST -H "Content-Type: application/json" -H "kbn-xsrf: anything" \
-          "${kibanaUrl}/api/saved_objects/index-pattern/$index_pattern" \
-          -d"{\"attributes\":{\"title\":\"$index_pattern\",\"timeFieldName\":\"@timestamp\"}}"
-        if [[ ${index_pattern} == "fluentd-*" ]] ; then
-            # Make fluentd the default index
-            echo
-            ${kibanaCurlPrefix} -X POST -H "Content-Type: application/json" -H "kbn-xsrf: anything" \
-              "${kibanaUrl}/api/kibana/settings/defaultIndex" \
-              -d"{\"value\":\"$index_pattern\"}"
-        fi
-    done
-    echo
-}
 
-function checkURL {
-    echo "${green}Waiting for URL $2${reset}"
-    until $(eval '$1 --output /dev/null --silent --head --fail $2'); do
-        printf '.'
-        sleep 5
+        url="$kibanaUrl/api/saved_objects/index-pattern/$index_pattern"
+        data="{\"attributes\":{\"title\":\"$index_pattern\",\"timeFieldName\":\"@timestamp\"}}"
+
+        ${kibanaCurlPrefix} --fail --data "$data" --request "$action" --header "$content_type" --header "$kbn_xsrf_header" ${url}
+
+        if [[ ${index_pattern} == "fluent-*" ]] ; then
+            # Make fluentd the default index
+            url="$kibanaUrl/api/kibana/settings/defaultIndex"
+            data="{\"value\":\"$index_pattern\"}"
+            echo
+            ${kibanaCurlPrefix} --data "$data" --request "$action" --header "$content_type" --header "$kbn_xsrf_header" ${url}
+        fi
     done
     echo
 }
--- scripts/reset-fluentd.sh
+++ scripts/reset-fluentd.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-DIRECTORY=$(cd `dirname $0` && pwd)
-source ${DIRECTORY}/helpers/common.sh
+directory=$(cd `dirname $0` && pwd)
+source ${directory}/helpers/common.sh
 
-docker-compose -p efk -f docker-compose.yml restart fluentd
-clearDockerLog fluentd
-
-while ! nc -z localhost 24224 </dev/null; do sleep 5; done
-
-docker logs -f fluentd
+restartContainer fluentd 24224
