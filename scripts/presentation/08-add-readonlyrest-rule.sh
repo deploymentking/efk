a938119cff181334986ddce8a2bc79cf7bf81550
Lee Myring <mail@thinkstack.io>
2018-06-25 18:45:54 +0100

    Fixes to presentation scripts post 1st run through

--- scripts/presentation/08-add-readonlyrest-rule.sh
+++ scripts/presentation/08-add-readonlyrest-rule.sh
@@ -3,4 +3,4 @@
 cd ~/source/deploymentking/
 
 cp -p fluentd-log-aggregation-my-sql/elasticsearch/config/readonlyrest.yml \
-    /fluentd-log-aggregation/elasticsearch/config/readonlyrest.yml
+    fluentd-log-aggregation/elasticsearch/config/readonlyrest.yml
