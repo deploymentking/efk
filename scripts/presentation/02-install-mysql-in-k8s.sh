b8c976afce50f5c99a5e360ca72a7b58a0371a71
Lee Myring <mail@thinkstack.io>
2018-06-26 21:40:01 +0100

    Add more useful output in scripts

--- scripts/presentation/02-install-mysql-in-k8s.sh
+++ scripts/presentation/02-install-mysql-in-k8s.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+directory=$(cd `dirname $0` && pwd)
+source ${directory}/../helpers/common.sh
+
 cd ../../
 
 kubectl create namespace database
 
 kubectl apply -f ./via-k8s/config/mysql.yaml
+
+echo
+echo "${green}Successfully launched MySQL container...${reset}"
